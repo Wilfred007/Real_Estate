5ee9c8d645bf9f85a5005d1d7e284871e6b0da2f
Wilfred007 <adzerwilfred007@gmail.com>
2025-08-08 21:40:30 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/sign-in' element={<Signin/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/Profile' element={<Profile/>} />
+
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
